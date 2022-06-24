3102ec2b139191993b2edbc48f2ae73928838029
Brayan <baviles599@gmail.com>
2022-06-23 22:09:46 -0500

    Exception handlers

--- src/main/java/com/revature/ePort/bid/BidController.java
+++ src/main/java/com/revature/ePort/bid/BidController.java
@@ -3,13 +3,20 @@ package com.revature.ePort.bid;
 import com.revature.ePort.bid.dtos.requests.NewBid;
 import com.revature.ePort.bid.dtos.responses.ViewBidHistory;
 import com.revature.ePort.util.annotations.Inject;
+import com.revature.ePort.util.custom_exception.AuthenticationException;
+import com.revature.ePort.util.custom_exception.InvalidRequestException;
+import com.revature.ePort.util.custom_exception.ResourceConflictException;
+import com.revature.ePort.util.custom_exception.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.View;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/bid")
@@ -38,5 +45,48 @@ public class BidController {
     @GetMapping(path = "/history", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody List<ViewBidHistory> bidHistoryList(@RequestBody String userID){
         return bidService.bidHistoryList(userID);
+    }
+
+    //region Exception Handlers
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public @ResponseBody
+    Map<String, Object> handleUnauthorizedException(UnauthorizedException e){
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("status", 401);
+        responseBody.put("message", e.getMessage());
+        responseBody.put("timestamp", LocalDateTime.now().toString());
+        return responseBody;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public @ResponseBody Map<String, Object> handleAuthenticationException(AuthenticationException e){
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("status", 403);
+        responseBody.put("message", e.getMessage());
+        responseBody.put("timestamp", LocalDateTime.now().toString());
+        return responseBody;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public @ResponseBody Map<String, Object> handleInvalidRequestException(InvalidRequestException e){
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("status", 404);
+        responseBody.put("message", e.getMessage());
+        responseBody.put("timestamp", LocalDateTime.now().toString());
+        return responseBody;
+    }
+
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public @ResponseBody Map<String, Object> handleResourceConflictException(ResourceConflictException e){
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("status", 409);
+        responseBody.put("message", e.getMessage());
+        responseBody.put("timestamp", LocalDateTime.now().toString());
+        return responseBody;
     }
 }
--- src/main/java/com/revature/ePort/order/OrderController.java
+++ src/main/java/com/revature/ePort/order/OrderController.java
@@ -3,10 +3,19 @@ package com.revature.ePort.order;
 
 import com.revature.ePort.order.dtos.requests.NewOrder;
 import com.revature.ePort.util.annotations.Inject;
+import com.revature.ePort.util.custom_exception.AuthenticationException;
+import com.revature.ePort.util.custom_exception.InvalidRequestException;
+import com.revature.ePort.util.custom_exception.ResourceConflictException;
+import com.revature.ePort.util.custom_exception.UnauthorizedException;
 import org.hibernate.engine.spi.ManagedEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/order")
@@ -25,5 +34,47 @@ public class OrderController {
     @ResponseBody String newOrder(@RequestBody NewOrder newOrder){
 
         return newOrder.getShipped_address();
+    }
+
+    //region Exception Handlers
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public @ResponseBody
+    Map<String, Object> handleUnauthorizedException(UnauthorizedException e){
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("status", 401);
+        responseBody.put("message", e.getMessage());
+        responseBody.put("timestamp", LocalDateTime.now().toString());
+        return responseBody;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public @ResponseBody Map<String, Object> handleAuthenticationException(AuthenticationException e){
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("status", 403);
+        responseBody.put("message", e.getMessage());
+        responseBody.put("timestamp", LocalDateTime.now().toString());
+        return responseBody;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public @ResponseBody Map<String, Object> handleInvalidRequestException(InvalidRequestException e){
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("status", 404);
+        responseBody.put("message", e.getMessage());
+        responseBody.put("timestamp", LocalDateTime.now().toString());
+        return responseBody;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public @ResponseBody Map<String, Object> handleResourceConflictException(ResourceConflictException e){
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("status", 409);
+        responseBody.put("message", e.getMessage());
+        responseBody.put("timestamp", LocalDateTime.now().toString());
+        return responseBody;
     }
 }
