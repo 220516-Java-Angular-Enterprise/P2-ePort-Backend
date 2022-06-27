7ab22d9aec84bc2cbb67ccea3fb5fe7d828f8372
Matthew Berkheimer <MatthewBerkheimerWork@gmail.com>
2022-06-27 07:43:03 -0700

    Global Cors added

---
+++ src/main/java/com/revature/ePort/util/cors/GlobCors.java
@@ -0,0 +1,23 @@
+package com.revature.ePort.util.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class GlobCors implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/ePort/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("PUT", "DELETE","GET","POST")
+                .allowedHeaders("header1", "header2", "header3","Authorization")
+                .exposedHeaders("Authorization")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
+
