be4b26fb7b45dde42acb4a74994e1ea34b526fb0
Brayan <baviles599@gmail.com>
2022-06-27 19:10:44 -0500

    base email files

---
+++ src/main/java/com/revature/ePort/util/email/Feedback.java
@@ -0,0 +1,40 @@
+package com.revature.ePort.util.email;
+
+import com.sun.istack.NotNull;
+
+
+public class Feedback {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String feedback;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}
