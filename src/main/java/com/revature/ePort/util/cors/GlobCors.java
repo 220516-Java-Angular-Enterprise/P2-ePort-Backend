cdd20d35acb59459f7504e2639902cfc94ebe73f
Matthew Berkheimer <MatthewBerkheimerWork@gmail.com>
2022-06-29 09:01:23 -0700

    fixed user services

--- src/main/java/com/revature/ePort/util/cors/GlobCors.java
+++ src/main/java/com/revature/ePort/util/cors/GlobCors.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @Configuration
 @EnableWebMvc
@@ -12,11 +12,12 @@ public class GlobCors implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/ePort/**")
-                .allowedMethods("PUT", "DELETE","GET","POST")
-                .allowedHeaders("header1", "header2", "header3","Authorization")
-                .exposedHeaders("Authorization")
-                .allowCredentials(false).maxAge(3600);
+        registry.addMapping("ePort/**")
+                .allowedOrigins("*")//"http://localhost:4200/"
+                .allowedMethods("PUT", "DELETE", "GET", "POST", "OPTIONS", "PATCH")
+                .allowedHeaders("header1", "header2", "header3","Authorization","content-type","access-control-allow-origin")
+                .exposedHeaders("header1", "header2", "Authorization", "content-type", "access-control-allow-origin");
+                //.allowCredentials(false).maxAge(3600);
     }
 }
 
